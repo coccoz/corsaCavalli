0f737608dc560956a72a7f838d5b551e152880ca
cosimo <ballerini.co@gmail.com>
2024-10-02 09:47:00 +0200

    test

---
+++ src/main/java/com/example/Cavallo.java
@@ -0,0 +1,14 @@
+public class Cavallo {
+    public run
+    {
+        int n;
+
+        for(int i=0, i<100 i++)
+        {
+            Random random = new Random();
+            n = random.nextInt(100);
+            System.out.ln("il cavallo " + this + " ha percorso " + i + " metri")
+            TimeUnit.MILLISECONDS.sleep(n);
+        }
+    }
+}
---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Cavallo c1 = new Cavallo;
+        Cavallo c2 = new Cavallo;
+        Cavallo c3 = new Cavallo;
+
+        c1.run();
+        c2.run();
+        c3.run();
+
+    }
+}
